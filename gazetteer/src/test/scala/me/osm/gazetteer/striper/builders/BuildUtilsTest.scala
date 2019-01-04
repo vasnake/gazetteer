dbff7791f9083194b397f7520f684e83612f8429
vasnake <vasnake@gmail.com>
2019-01-04 20:59:18 +0300

    add multipolygon builder tests

--- gazetteer/src/test/scala/me/osm/gazetteer/striper/builders/BuildUtilsTest.scala
+++ gazetteer/src/test/scala/me/osm/gazetteer/striper/builders/BuildUtilsTest.scala
@@ -8,7 +8,8 @@ import com.vividsolutions.jts.io.{WKTReader, WKTWriter}
 
 import me.osm.gazetteer.striper.readers.RelationsReader
 
-import collection.JavaConverters._
+import scala.collection.JavaConverters._
+import scala.language.implicitConversions
 
 
 class BuildUtilsTest extends FlatSpec with Matchers {
@@ -23,6 +24,12 @@ class BuildUtilsTest extends FlatSpec with Matchers {
     // todo: geojson?
     private val wktwriter = new WKTWriter()
     private val wktreader = new WKTReader(gf)
+
+    // default OSM relation
+    private val relstub = new RelationsReader.Relation { id = 42 }
+
+    // default compare tolerance
+    private val tolerance = 0.0001
 
     private def geometries(geomColl: GeometryCollection): Iterable[Geometry] = {
         val res = for {
@@ -45,11 +52,34 @@ class BuildUtilsTest extends FlatSpec with Matchers {
         res
     }
 
+    def loadWaysFromText(txt: String): Iterable[LineString] = {
+        import Toolbox.StringOps
+
+        def buildCoordinate(xy: Seq[String]): Coordinate = {
+            new Coordinate(xy(0).toDouble, xy(1).toDouble)
+        }
+
+        def loadLineString(str: String): LineString = {
+            val coords = str.toArray(",").map(xy => {
+                buildCoordinate(xy.toList(" "))
+            })
+
+            gf.createLineString(coords)
+        }
+
+        txt.toList("\n").map(s => loadLineString(s))
+    }
+
+    def ringEqual(left: LineString, right: String): Boolean = {
+        // assert(ringEqual(polygonInners(g.getGeometryN(0)).head, "30 20, 20 15, 20 25"))
+        ???
+    }
+
     implicit def iterable2jul[T](it: Iterable[T]): java.util.List[T] = it.toList.asJava
 
-    // gazetteer/testOnly me.osm.gazetteer.striper.builders.BuildUtilsTest -- -z "multipolygon"
-    it should "build multipolygon" in {
-        val relstub = new RelationsReader.Relation { id = 42 }
+    // gazetteer/testOnly me.osm.gazetteer.striper.builders.BuildUtilsTest -- -z "multipolygon from WKT"
+    it should "build multipolygon from WKT" in {
+        // define input
         val mpolywkt =
             """
               |MULTIPOLYGON ((
@@ -57,22 +87,115 @@ class BuildUtilsTest extends FlatSpec with Matchers {
               | (30 20, 20 15, 20 25, 30 20)
               |))
             """.stripMargin.trim.replaceAll("\n", "")
+
+        // load input
         val mpoly = wktreader.read(mpolywkt).asInstanceOf[MultiPolygon]
         assert(wktwriter.write(mpoly) === mpolywkt)
-
         val polygons = geometries(mpoly)
         val outers = polygons.map(p => polygonOuter(p))
         val inners = polygons.flatMap(p => polygonInners(p))
 
+        // generate output
         val g = BuildUtils.buildMultyPolygon(logger, relstub, outers, inners)
             .reverse()
         val wkt = wktwriter.write(g)
 
+        // check output
         println("RES: " + wkt); println("SRC: " + mpolywkt)
         println(s"area: ${g.getArea}")
 
         assert(wkt === mpolywkt)
         assert(mpoly.getArea === g.getArea)
-        assert(mpoly.getCentroid.equalsExact(g.getCentroid, 0.0001))
+        assert(mpoly.getCentroid.equalsExact(g.getCentroid, tolerance))
+    }
+
+    // gazetteer/testOnly me.osm.gazetteer.striper.builders.BuildUtilsTest -- -z "multipolygon from text"
+    it should "build multipolygon from text" in {
+        // define input
+        val outerWays =
+            """
+              |20 35, 10 10, 45 20, 20 35
+            """.stripMargin.trim
+        val innerWays =
+            """
+              |30 20, 20 15, 20 25, 30 20
+            """.stripMargin.trim
+
+        // load input
+        val outers = loadWaysFromText(outerWays)
+        val inners = loadWaysFromText(innerWays)
+
+        // generate output
+        val g = BuildUtils.buildMultyPolygon(logger, relstub, outers, inners)
+            .reverse()
+        val wkt = wktwriter.write(g)
+
+        // check output
+        assert(wkt === "MULTIPOLYGON (((20 35, 10 10, 45 20, 20 35), (30 20, 20 15, 20 25, 30 20)))")
+        assert(g.getArea === (337.5 +- tolerance))
+        assert(g.getCentroid.getX === (25.2469 +- tolerance))
+        assert(g.getCentroid.getY === (21.9135 +- tolerance))
+    }
+
+    // gazetteer/testOnly me.osm.gazetteer.striper.builders.BuildUtilsTest -- -z "multipolygon from multi text"
+    it should "build multipolygon from multi text" in {
+        // define input
+        val outerWays =
+            """
+              |20 35, 10 10
+              |10 10, 45 20
+              |45 20, 20 35
+            """.stripMargin.trim
+        val innerWays =
+            """
+              |30 20, 20 15
+              |20 15, 20 25
+              |20 25, 30 20
+            """.stripMargin.trim
+
+        // load input
+        val outers = loadWaysFromText(outerWays)
+        val inners = loadWaysFromText(innerWays)
+
+        // generate output
+        val g = BuildUtils.buildMultyPolygon(logger, relstub, outers, inners)
+            .reverse()
+        val wkt = wktwriter.write(g)
+        println("RES: " + wkt)
+
+        // check output
+//        assert(ringEqual(polygonOuter(g.getGeometryN(0)), "20 35, 10 10, 45 20"))
+//        assert(ringEqual(polygonInners(g.getGeometryN(0)).head, "30 20, 20 15, 20 25"))
+        assert(g.getArea === (337.5 +- tolerance))
+        assert(g.getCentroid.getX === (25.2469 +- tolerance))
+        assert(g.getCentroid.getY === (21.9135 +- tolerance))
+    }
+}
+
+object Toolbox {
+
+    implicit class StringOps(val src: String) extends AnyVal {
+
+        /**
+          * trim src, split src by regexp(sep surrounded by blanks), drop empty items
+          */
+        def toArray(implicit sep: String = ","): Array[String] =
+            src.trim.split("""\s*""" + sep + """\s*""").filterNot(_.isEmpty)
+
+        /**
+          * split string by sep, trim every item, convert array to list, drop empty items
+          */
+        def toList(sep: String): Seq[String] = src.split(sep).map(_.trim).toSeq.filter(_.nonEmpty)
+
+        /**
+          * Create Map from string, e.g. "foo: bar; poo: bazz"
+          */
+        def loadMap(implicit pairsSep: String = ";", kvSep: String = ":"): Map[String, String] = {
+            src.toArray(pairsSep)
+                .flatMap(s => s.toArray(kvSep) match {
+                    case Array(k, v) => Some(k -> v)
+                    case _ => None
+                } ).toMap
+        }
     }
 }
