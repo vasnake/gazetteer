bea14217a0d54df8333da57de42cc70a52490438
vasnake <vasnake@gmail.com>
2019-01-05 00:56:20 +0300

    add tests for build multipolygon

--- gazetteer/src/test/scala/me/osm/gazetteer/striper/builders/BuildUtilsTest.scala
+++ gazetteer/src/test/scala/me/osm/gazetteer/striper/builders/BuildUtilsTest.scala
@@ -3,13 +3,14 @@ package me.osm.gazetteer.striper.builders
 import org.slf4j.LoggerFactory
 import org.scalatest._
 
+import scala.annotation.tailrec
+import scala.collection.JavaConverters._
+import scala.language.implicitConversions
+
 import com.vividsolutions.jts.geom._
 import com.vividsolutions.jts.io.{WKTReader, WKTWriter}
 
 import me.osm.gazetteer.striper.readers.RelationsReader
-
-import scala.collection.JavaConverters._
-import scala.language.implicitConversions
 
 
 class BuildUtilsTest extends FlatSpec with Matchers {
@@ -19,7 +20,7 @@ class BuildUtilsTest extends FlatSpec with Matchers {
     private val WGS84 = 4326
 
     // fixed precision model
-    private val gf = new GeometryFactory(new PrecisionModel(10000), WGS84)
+    private implicit val gf: GeometryFactory = new GeometryFactory(new PrecisionModel(10000), WGS84)
 
     // todo: geojson?
     private val wktwriter = new WKTWriter()
@@ -29,7 +30,7 @@ class BuildUtilsTest extends FlatSpec with Matchers {
     private val relstub = new RelationsReader.Relation { id = 42 }
 
     // default compare tolerance
-    private val tolerance = 0.0001
+    private implicit val tolerance: Double = 0.0001
 
     private def geometries(geomColl: GeometryCollection): Iterable[Geometry] = {
         val res = for {
@@ -52,27 +53,48 @@ class BuildUtilsTest extends FlatSpec with Matchers {
         res
     }
 
-    def loadWaysFromText(txt: String): Iterable[LineString] = {
-        import Toolbox.StringOps
+    import Toolbox.StringOps
 
-        def buildCoordinate(xy: Seq[String]): Coordinate = {
-            new Coordinate(xy(0).toDouble, xy(1).toDouble)
-        }
+    private def buildCoordinate(xy: Seq[String]): Coordinate = {
+        new Coordinate(xy(0).toDouble, xy(1).toDouble)
+    }
 
-        def loadLineString(str: String): LineString = {
-            val coords = str.toArray(",").map(xy => {
-                buildCoordinate(xy.toList(" "))
-            })
+    private def loadLineString(str: String)(implicit gf: GeometryFactory): LineString = {
+        val coords = str.toArray(",").map(xy => {
+            buildCoordinate(xy.toList(" "))
+        })
 
-            gf.createLineString(coords)
-        }
+        gf.createLineString(coords)
+    }
 
+    private def loadWaysFromText(txt: String): Iterable[LineString] = {
         txt.toList("\n").map(s => loadLineString(s))
     }
 
-    def ringEqual(left: LineString, right: String): Boolean = {
-        // assert(ringEqual(polygonInners(g.getGeometryN(0)).head, "30 20, 20 15, 20 25"))
-        ???
+    private def ringEqual(left: LineString, right: String)(implicit tolerance: Double): Boolean = {
+        val leftPoints = left.getCoordinates.dropRight(1)
+        val rightPoints = loadLineString(right).getCoordinates
+
+        def pointsEqual(p1: Coordinate, p2: Coordinate): Boolean = {
+            p1.x === (p2.x +- tolerance) &&
+                p1.y === (p2.y +- tolerance)
+        }
+
+        @tailrec
+        def checkPairs(left: Array[Coordinate], right: Array[Coordinate], cnt: Int): Boolean = {
+            if (cnt <= 0) false
+            else {
+                val pairs = left.zip(right)
+                if (pairs.forall(p => pointsEqual(p._1, p._2))) true
+                else checkPairs(left.tail :+ left.head, right, cnt - 1)
+            }
+        }
+
+        if (leftPoints.length == rightPoints.length) {
+            require(leftPoints.length > 2, "ring must have 3 points at least")
+            checkPairs(leftPoints, rightPoints, leftPoints.length)
+        }
+        else false
     }
 
     implicit def iterable2jul[T](it: Iterable[T]): java.util.List[T] = it.toList.asJava
@@ -101,9 +123,7 @@ class BuildUtilsTest extends FlatSpec with Matchers {
         val wkt = wktwriter.write(g)
 
         // check output
-        println("RES: " + wkt); println("SRC: " + mpolywkt)
-        println(s"area: ${g.getArea}")
-
+        println("SRC: " + mpolywkt); println("RES: " + wkt)
         assert(wkt === mpolywkt)
         assert(mpoly.getArea === g.getArea)
         assert(mpoly.getCentroid.equalsExact(g.getCentroid, tolerance))
@@ -164,8 +184,8 @@ class BuildUtilsTest extends FlatSpec with Matchers {
         println("RES: " + wkt)
 
         // check output
-//        assert(ringEqual(polygonOuter(g.getGeometryN(0)), "20 35, 10 10, 45 20"))
-//        assert(ringEqual(polygonInners(g.getGeometryN(0)).head, "30 20, 20 15, 20 25"))
+        assert(ringEqual(polygonOuter(g.getGeometryN(0)), "20 35, 10 10, 45 20"))
+        assert(ringEqual(polygonInners(g.getGeometryN(0)).head, "30 20, 20 15, 20 25"))
         assert(g.getArea === (337.5 +- tolerance))
         assert(g.getCentroid.getX === (25.2469 +- tolerance))
         assert(g.getCentroid.getY === (21.9135 +- tolerance))
